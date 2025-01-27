728950eb835a8811fd6166169c4ed0101df37e52
akshitaarora26 <akshita.arora2699@gmail.com>
2025-01-26 19:43:00 -0600

    portfolio

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// This file can be used to add interactivity, animations, or dynamic content to your page.
+// Example: Smooth scrolling between sections
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
